17f40a508248622932b9346e4f6c06876447f9e4
rkotrys <robert.kotrys@gmail.com>
2018-07-23 21:42:05 +0200

    20180723-2

---
+++ page1/js/stafflist.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $(".staff-card").hover(
+        function(){ $(this).css("background-color", "#fff;"); }, 
+        function(){ $(this).css("background-color", "rgba(240,250,240,90%)"  ); }
+    );
+
+
+
+}
